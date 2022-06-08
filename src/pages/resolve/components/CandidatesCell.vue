4e0ceefed5f10d51e7f571541a0f421c6d56a7ab
kylanhurt <kylan.hurt@gmail.com>
2022-06-08 18:32:54 +0200

    First implementation of Elections Table with CandidatesCell

---
+++ src/pages/resolve/components/CandidatesCell.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-td key="candidates" :props="props">
+    {{ candidatesSyntax }}
+  </q-td>
+</template>
+
+<script>
+export default {
+  props: ['props'],
+  mounted: function () {
+  },
+  computed: {
+    candidatesSyntax () {
+      const candidateNames = this.props.row.candidates.map(item => item.name)
+      const candidateSyntax = candidateNames.join(', ')
+      return candidateSyntax
+    }
+  }
+}
+</script>
--- src/pages/resolve/components/ElectionsTable.vue
+++ src/pages/resolve/components/ElectionsTable.vue
@@ -1,16 +1,52 @@
 <template>
   <div class="q-pa-md">
-    <q-table title="Treats" :data="electionData" :columns="columns" row-key="name" />
+    <q-table title="Elections" :data="electionData" :columns="columns" row-key="name">
+      <template v-slot:body="props">
+        <q-tr :props="props">
+          <q-td key="election_id" :props="props">
+            {{ props.row.election_id }}
+          </q-td>
+          <q-td key="ballot_name" :props="props">
+            {{ props.row.ballot_name }}
+          </q-td>
+          <q-td key="info_url" :props="props">
+            {{ props.row.info_url }}
+          </q-td>
+          <candidates-cell :props="props"></candidates-cell>
+          <q-td key="available_seats" :props="props">
+            {{ props.row.available_seats }}
+          </q-td>
+          <q-td key="begin_voting_ts" :props="props">
+            {{ props.row.begin_voting_ts }}
+          </q-td>
+          <q-td key="end_voting_ts" :props="props">
+            {{ props.row.end_voting_ts }}
+          </q-td>
+        </q-tr>
+      </template>
+    </q-table>
   </div>
 </template>
 
 <script>
 import { GET_TABLE_ROWS } from '../util/fetch'
+import CandidatesCell from './CandidatesCell.vue'
 
 export default {
+  components: {
+    CandidatesCell
+  },
   data () {
     return {
-      columns: [],
+      columns: [
+        { name: 'election_id', label: 'ID', field: 'election_id' },
+        { name: 'ballot_name', label: 'Ballot Name', field: 'ballot_name' },
+        { name: 'info_url', label: 'Info', field: 'info_url' },
+        { name: 'candidates', label: 'Candidates', field: 'candidates' },
+        { name: 'available_seats', label: 'Seats', field: 'available_seats' },
+        { name: 'begin_voting_ts', label: 'Start Voting', field: 'begin_voting_ts' },
+        { name: 'end_voting_ts', label: 'End Voting', field: 'end_voting_ts' }
+      ],
       electionData: []
     }
   },
@@ -18,12 +54,13 @@ export default {
     async fetchElections () {
       console.log('fetching elections')
       try {
-        const data = await GET_TABLE_ROWS({
+        const { rows } = await GET_TABLE_ROWS({
           code: 'testtelosarb',
           scope: 'testtelosarb',
           table: 'elections'
         })
-        this.electionData = data
+        // console.log('fetchElections data: ', data)
+        this.electionData = rows
       } catch (err) {
         console.warn('fetchElections error: ', err)
       }
