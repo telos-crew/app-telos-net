7b38c271a79706126b866cab37d4f4bf50d45df8
kylanhurt <kylan.hurt@gmail.com>
2022-06-16 11:14:00 -0700

    Some improvements to ArbitratorsTable

--- src/pages/resolve/components/ArbitratorsTable.vue
+++ src/pages/resolve/components/ArbitratorsTable.vue
@@ -6,12 +6,39 @@
       :columns="columns"
       row-key="name"
     >
+      <template v-slot:body-cell-arb="props">
+        <q-td :props="props">
+					<profile-avatar :account_name="props.arb" size="24px" childClass="profile-avatar"></profile-avatar>
+						<span>&nbsp;{{props.row.arb}}</span>
+					</q-td>
+			</template>
+      <template v-slot:body-cell-open_case_ids="props">
+        <q-td :props="props">
+						<span v-for="id in props.row.open_case_ids" v-bind:key="id">{{id}}<br /></span>
+					</q-td>
+			</template>
+      <template v-slot:body-cell-closed_case_ids="props">
+        <q-td :props="props">
+						<span v-for="id in props.row.closed_case_ids" v-bind:key="id">{{id}}<br /></span>
+					</q-td>
+			</template>
+      <template v-slot:body-cell-arb_status="props">
+        <q-td :props="props">
+						<span>{{getArbStatus(props.row.arb_status)}}</span>
+					</q-td>
+			</template>
     </q-table>
   </div>
 </template>
 
 <script>
+import ProfileAvatar from 'src/components/common/ProfileAvatar.vue'
+import { ARBITRATOR_STATUS_LIST } from '../constants/arbitrators'
+
 export default {
+  components: {
+    ProfileAvatar
+  },
   data () {
     return {
       arbitratorsData: [],
@@ -28,6 +55,9 @@ export default {
     }
   },
   methods: {
+    getArbStatus (statusCode) {
+      return ARBITRATOR_STATUS_LIST[statusCode]
+    },
     async fetchArbitrators () {
       try {
         const { rows } = await this.$store.$api.getTableRows({
