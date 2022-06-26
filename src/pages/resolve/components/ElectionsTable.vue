0724ecc3a244c6c0d7d60118a1cddbd6b3bc3d51
kylanhurt <kylan.hurt@gmail.com>
2022-06-27 00:53:29 +0200

    Show end_add_candidates_ts in Elections table

--- src/pages/resolve/components/ElectionsTable.vue
+++ src/pages/resolve/components/ElectionsTable.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="q-pa-md">
-    <q-table title="Elections" :data="electionData" :columns="columns" row-key="name">
+    <q-table title="Elections"
+      :data="electionData"
+      :columns="columns"
+      row-key="name"
+      :loading="electionData === null"
+    >
       <template v-slot:body="props">
         <q-tr :props="props">
           <q-td key="election_id" :props="props">
@@ -17,6 +22,9 @@
           <candidates-cell :props="props"></candidates-cell>
           <q-td key="available_seats" :props="props">
             {{ props.row.available_seats }}
+          </q-td>
+          <q-td key="end_add_candidates_ts" :props="props">
+            {{ props.row.end_add_candidates_ts }}
           </q-td>
           <q-td key="begin_voting_ts" :props="props">
             {{ props.row.begin_voting_ts }}
@@ -45,18 +53,23 @@ export default {
   },
   data () {
     return {
-      electionData: this.$store.state.resolve.elections,
       columns: [
         { name: 'election_id', label: 'ID', field: 'election_id' },
         { name: 'ballot_name', label: 'Ballot Name', field: 'ballot_name' },
         { name: 'info_url', label: 'Info', field: 'info_url' },
         { name: 'candidates', label: 'Candidates', field: 'candidates' },
         { name: 'available_seats', label: 'Seats', field: 'available_seats' },
+        { name: 'end_add_candidates_ts', label: 'End Add Candidates', field: 'end_add_candidates_ts' },
         { name: 'begin_voting_ts', label: 'Start Voting', field: 'begin_voting_ts' },
         { name: 'end_voting_ts', label: 'End Voting', field: 'end_voting_ts' },
         { name: 'status', label: 'Status', field: 'status' }
       ],
       ELECTION_STATUS
+    }
+  },
+  computed: {
+    electionData () {
+      return this.$store.state.resolve.elections
     }
   }
 }
