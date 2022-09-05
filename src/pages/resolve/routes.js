d15bc202adcf492860f2637a0bc03c27a987bacf
kylanhurt <kylan.hurt@gmail.com>
2022-09-06 00:08:46 +0100

    Removec reference to obsolete Resolve home route

--- src/pages/resolve/routes.js
+++ src/pages/resolve/routes.js
@@ -1,10 +1,8 @@
 import Elections from './pages/elections/Elections.vue'
-// import Home_old from './pages/home/Home_old.vue.vue'
 import Home from './pages/home/Home.vue'
 import CaseFile from './pages/case-file/CaseFile.vue'
 
 export const resolveRoutes = [
-  // { path: '/_old', component: Home_old },
   { path: '/', component: Home },
   { path: 'elections', component: Elections },
   { path: 'case-file/:id', component: CaseFile }
