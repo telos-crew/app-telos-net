3665bd2a8a9c3ff50096f73e235d3f7866fa3004
kylanhurt <kylan.hurt@gmail.com>
2022-09-07 16:20:42 +0100

    Add Roboto font

--- src/pages/resolve/pages/elections/Elections.vue
+++ src/pages/resolve/pages/elections/Elections.vue
@@ -1,20 +1,49 @@
 <template>
   <div>
-    <nominees-table></nominees-table>
+    <!-- <nominees-table></nominees-table> -->
+    <div class="intro">
+      <section class="info">
+        <h3>Ensure Telos is fair</h3>
+        <p class="second">Do you have an asset related issue that you’d like the network’s arbitration system to resolve? We believe that a transparent process benefits everyone.</p>
+        <q-btn color="primary" label="Create a new case" />
+      </section>
+    </div>
     <elections-table></elections-table>
   </div>
 </template>
 
 <script>
 import ElectionsTable from '../../components/ElectionsTable.vue'
-import NomineesTable from '../../components/NomineesTable.vue'
+// import NomineesTable from '../../components/NomineesTable.vue'
 
 export default {
   components: {
-    ElectionsTable,
-    NomineesTable
+    ElectionsTable
+    // NomineesTable
   }
 }
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+	.info {
+		margin-right: 80px;
+    width: 40%;
+    margin-bottom: 38px;
+
+		h3 {
+			font-style: normal;
+			font-weight: 600;
+			font-size: 24px;
+			line-height: 37px;
+			color: rgba(51, 51, 51, 0.5);
+		}
+		p.second {
+			font-style: normal;
+			font-weight: 400;
+			font-size: 14px;
+			line-height: 18px;
+			color: #333333;
+			margin-bottom: 36px;
+		}
+	}
+</style>
